e32e854c68575a7621abc6e267911c575a32766a
Artyom Eryomkin <43377589+RakePants@users.noreply.github.com>
2024-09-15 02:29:59 +0500

    Remove redundant imports

--- backend/generation/src/api.py
+++ backend/generation/src/api.py
@@ -1,6 +1,5 @@
 import uvicorn
 from fastapi import FastAPI, Response
-from fastapi.responses import FileResponse
 from generation.src.creator.create import create
 from generation.src.schemas import GenerationRequest
 
@@ -10,11 +9,13 @@ app = FastAPI()
 @app.post("/create")
 async def create_route(generation_request: GenerationRequest) -> Response:
     html_file = await create(generation_request.document_name, generation_request.query)
-    
+
     return Response(
-        content=html_file.getvalue(), 
-        media_type="text/html", 
-        headers={"Content-Disposition": f"attachment; filename={generation_request.document_name.split('.')[0] + '.html'}"}
+        content=html_file.getvalue(),
+        media_type="text/html",
+        headers={
+            "Content-Disposition": f"attachment; filename={generation_request.document_name.split('.')[0] + '.html'}"
+        },
     )
 
 
