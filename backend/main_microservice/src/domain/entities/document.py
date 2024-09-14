2d215bfb2e6e0a1ee915dba35999dfb1aff4df3f
Maxim Osetrov <mosetrov@samokat.ru>
2024-09-14 14:37:32 +0300

    Code refactoring and added mongo repository

--- backend/main_microservice/src/domain/entities/document.py
+++ backend/main_microservice/src/domain/entities/document.py
@@ -11,13 +11,13 @@ from domain.values.file_type import FileType
 class Document(BaseEntity):
     document: DocumentFile
     file_type: FileType
-    first_user: User
-    second_user: User
+    customer: User
+    performer: User
     versions: set[Version] = field(default_factory=set, kw_only=True)
 
     @classmethod
     def create_new_document(cls, document: DocumentFile, file_type: FileType, first_user: User, second_user: User) -> "Document":
-        return cls(document=document, file_type=file_type, first_user=first_user, second_user=second_user)
+        return cls(document=document, file_type=file_type, customer=first_user, performer=second_user)
 
     def add_new_version(self, version: Version) -> None:
         self.versions.add(version)
--- backend/main_microservice/src/infrastructure/repository/documents/mongo.py
+++ backend/main_microservice/src/infrastructure/repository/documents/mongo.py
@@ -1,7 +1,10 @@
 from abc import ABC
 from dataclasses import dataclass
-from motor.core import AgnosticClient, AgnosticCollection
+from motor.core import AgnosticClient, AgnosticCollection, AgnosticDatabase
+from motor.motor_asyncio import AsyncIOMotorClient
 
+from domain.entities.document import Document
+from domain.entities.user import User
 from infrastructure.repository.documents.base import BaseDocumentRepository
 
 
@@ -10,6 +13,9 @@ class BaseMongoDBRepository(ABC):
     mongo_db_client: AgnosticClient
     mongo_db_db_name: str
 
+    def _db(self) -> AgnosticDatabase:
+        return self.mongo_db_client[self.mongo_db_db_name]
+
     def _collection(self, collection_name: str) -> AgnosticCollection:
         return self.mongo_db_client[self.mongo_db_db_name][collection_name]
 
@@ -17,4 +23,10 @@ class BaseMongoDBRepository(ABC):
 @dataclass
 class MongoDocumentRepository(BaseDocumentRepository, BaseMongoDBRepository):
 
-    def create_new_document(self):
+    async def create_new_document(self, document: Document, customer: User, performer: User) -> AgnosticCollection:
+        return await self._db().create_collection(name=str(document.id), customer=customer.telegram_id, performer=performer.telegram_id)
+
+
+if __name__ == '__main__':
+    rep = MongoDocumentRepository(AsyncIOMotorClient(host="localhost", port=27017), "documents")
+    print(rep)
