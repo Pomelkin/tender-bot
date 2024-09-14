5525e54f67f731f9886e45da1059f58f458b171e
Emelyan Egorov <113930312+Pomelkin@users.noreply.github.com>
2024-09-14 18:11:27 +0500

    Add warmup logging, fix docstrings

--- backend/embeddings/api.py
+++ backend/embeddings/api.py
@@ -1,5 +1,6 @@
 import litserve as ls
 import torch
+from embeddings.utils import LOGGER
 from embeddings.schemas import EmbeddingsRequest, Embedding, EmbeddingsResponse
 from embeddings.config import settings
 from transformers import AutoTokenizer, AutoModel
@@ -9,6 +10,7 @@ class EmbeddingsAPI(ls.LitAPI):
     def setup(self, device: str):
         """
         Load the embedding model and tokenizer, also warm up the model.
+
         :param device: device to run the model on.
         """
         self.model = AutoModel.from_pretrained(settings.model_name).to(device)
@@ -16,7 +18,10 @@ class EmbeddingsAPI(ls.LitAPI):
         self.device = device
 
         self.model.eval()
+
+        LOGGER.info(f"Warmup the model {settings.model_name}")
         self.warm_up()
+        LOGGER.info(f"Model {settings.model_name} is ready to serve")
 
     def warm_up(self) -> None:
         """
@@ -39,6 +44,7 @@ class EmbeddingsAPI(ls.LitAPI):
     def decode_request(self, request: EmbeddingsRequest, **kwargs) -> list:
         """
         Decode the request payload to the model input.
+
         :param request: request payload.
         :return: model input.
         """
@@ -49,6 +55,9 @@ class EmbeddingsAPI(ls.LitAPI):
     def predict(self, x, **kwargs):
         """
         Run the model on the input and return the output.
+
+        :param x: model input.
+        :return: model output.
         """
         encoded_input = self.tokenizer(
             x, padding=True, truncation=True, return_tensors="pt"
@@ -60,6 +69,9 @@ class EmbeddingsAPI(ls.LitAPI):
     def encode_response(self, output, **kwargs) -> dict:
         """
         Encode embedding output.
+
+        :param output: model output.
+        :return: response payload.
         """
         embeddings = []
         for index, embedding in enumerate(output):
